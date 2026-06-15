979c64d48ab00500e067f9a989d0491e56fb64e7
giovaneramos <giovane_pr@hotmail.com>
2026-06-15 16:19:21 -0300

    Add service worker cache

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'eventpay-cache-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './css/style.css',
+  './css/simulator.css',
+  './js/utils.js',
+  './js/app.js',
+  './js/simulator.js',
+  './data/mock-data.js',
+  './services/storage.js',
+  './services/sync-engine.js',
+  './services/domain.js',
+  './components/views.js',
+  './simulador-smartpos.html',
+  './assets/icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const cloned = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, cloned));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
+  );
+});
